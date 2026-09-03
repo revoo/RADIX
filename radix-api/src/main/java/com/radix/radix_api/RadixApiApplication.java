001ac4a59e82792d0543ff6e297f4d1721289e0b
Russ <access2500@protonmail.com>
2026-09-03 15:31:02 -0500

    Spring init on radix-api

---
+++ radix-api/src/main/java/com/radix/radix_api/RadixApiApplication.java
@@ -0,0 +1,13 @@
+package com.radix.radix_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadixApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadixApiApplication.class, args);
+	}
+
+}
---
+++ radix-api/src/test/java/com/radix/radix_api/RadixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radix.radix_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadixApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
